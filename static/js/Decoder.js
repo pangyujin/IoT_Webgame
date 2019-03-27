86df83decfb66e9c7fef9f7f4f4d42e30844dcd0
pangyujinMBP <pangyujin98@gmail.com>
2019-03-27 11:39:12 +0800

    modify fonts and modularize the js function

---
+++ static/js/Decoder.js
@@ -0,0 +1,27 @@
+var InverseCaesarShift = function(str, key) {
+  var output = '';
+  for (var i=0; i<str.length; i++) {
+    var c = str[i];
+    if (c.match(/[a-z]/i)) {
+      var code = str.charCodeAt(i);
+      if ((code >= 65) && (code <= 90))
+        c = String.fromCharCode((code - 65 + 26 - key) % 26 + 65);
+      else if ((code >= 97) && (code <= 122))
+        c = String.fromCharCode((code - 97 + 26 - key) % 26 + 97);
+    }
+    else if (c === ' ')
+      c = ' ';
+    else
+      return "Not valid input";
+    output += c;
+  }
+  return output;
+}
+function decode() {
+  var dl = document.getElementById("droplist");
+  var key = dl.options[dl.selectedIndex].value;
+  var cw = document.getElementById("cw").value;
+  result = InverseCaesarShift(cw, key);
+  // console.log(result)
+  document.getElementById("result").value = result;
+}
